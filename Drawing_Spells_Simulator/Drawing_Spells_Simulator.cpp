7d25983b6452994240dee2ee5f3f6d6d43470638
Wallace Ting <wallaceting29@gmail.com>
2025-05-30 23:50:11 +0800

    Add project files.

---
+++ Drawing_Spells_Simulator/Drawing_Spells_Simulator.cpp
@@ -0,0 +1,9 @@
+﻿#include "Drawing_Spells_Simulator.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
---
+++ Drawing_Spells_Simulator/Drawing_Spells_Simulator.h
@@ -0,0 +1,8 @@
+﻿// Drawing_Spells_Simulator.h : Include file for standard system include files,
+// or project specific include files.
+
+#pragma once
+
+#include <iostream>
+
+// TODO: Reference additional headers your program requires here.
